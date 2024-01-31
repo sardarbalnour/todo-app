55e8f21be0bf628f74eb8aaf462208a6f06e07f3
Sardar Balnour <balnour.sardar@gmail.com>
2024-01-31 16:29:06 +0330

    getting todo values.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+const addButton = document.querySelector("#add-button");
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+
+const todos = [];
+// getting todo values
+const addHandler = () => {
+  const task = taskInput.value;
+  const date = dateInput.value;
+  const todo = {
+    task: task,
+    date: date,
+    completed: false,
+  };
+  if (task) {
+    todos.push(todo);
+    taskInput.value=("");
+    dateInput.value=("");
+    console.log(todos);
+  } else {
+    alert("warning")
+  }
+};
+addButton.addEventListener("click", addHandler);
+
