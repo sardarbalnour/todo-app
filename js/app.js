074c8f7daeec7696faecacd6a42a8b387bdeec31
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-08 10:58:45 +0330

    Edit todos finished

--- js/app.js
+++ js/app.js
@@ -134,8 +134,24 @@ const editHandler = (id) => {
   dateInput.value = todo.date;
   addButton.style.display = "none";
   editButton.style.display = "inline-block";
+  editButton.dataset.id = id;
+};
+
+const applyEditHandler = (event) => {
+  const id = event.target.dataset.id;
+  const todo = todos.find((todo) => todo.id === id);
+  todo.task = taskInput.value;
+  todo.date = dateInput.value;
+  taskInput.value = "";
+  dateInput.value = "";
+  addButton.style.display = "inline-block";
+  editButton.style.display = "none";
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo edited successfully", "success")
 };
 
 window.addEventListener("load", displayTodos);
 addButton.addEventListener("click", addHandler);
 deleteAllButton.addEventListener("click", deleteAllHandler);
+editButton.addEventListener("click", applyEditHandler);
